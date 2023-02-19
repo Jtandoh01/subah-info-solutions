700caf9c18b6e662333424ca825c7f169e6c7dd9
Mickon <jtandoh010@st.ug.edu.gh>
2023-02-19 14:33:16 +0000

    first comment

---
+++ README.md
@@ -0,0 +1,16 @@
+# School Management System Backend Service
+
+This is a backend service for a school management system, implemented using Node.js and MongoDB.
+
+## Getting Started
+
+1. Clone the repository: `git clone https://github.com/Jtandoh01/backend-project.git`
+2. Install the dependencies: `npm install`
+3. Create a `.env` file with the MongoDB connection string
+4. Start the server.js file with npm start
+
+## API Endpoints
+
+- `GET /api/student/:studentId`: Retrieve student details by student id
+- `POST /api/student`: Register a new student by supplying student details in the request body
+
---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+
+const studentRoutes = require('./routes/student');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+// Middleware to parse request body as JSON
+app.use(express.json());
+
+// Endpoint to retrieve student details by ID
+app.get('/students/:studentId', (req, res) => {
+  // Implementation to retrieve student details
+  res.status(200).send('HELLO, YOUR STUDENT ID IS: '+ req.params.studentId);
+});
+
+// Endpoint to register new student
+app.post('/students', (req, res) => {
+  // Implementation to register new student
+  res.status(200).send('HELLO, YOU ARE ASSIGNED THE STUDENT ID ' + req.body.studentId+', ' +' CONGRATULATIONS .');
+});
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
