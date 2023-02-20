9ac9b59b205b13105d5f7a1519df66bc3993c757
Mickon <jtandoh010@st.ug.edu.gh>
2023-02-20 07:02:12 +0000

    firs commit

--- route/studentRoutes.js
+++ route/studentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Student = require('../models/student');
+const Student = require('../student');
 
 // GET request for retrieving student details by student id
 
